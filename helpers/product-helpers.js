01ff06bed37774ffee455b377a051a89f6e6b598
Effin Joe Wilson <effinjoewilson@gmail.com>
2022-10-23 12:40:36 +0530

    delete product for admin

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,6 +1,7 @@
 
 var db = require('../config/connection')
 var collection= require('../config/collections')
+var objectId=require('mongodb').ObjectId
 
 module.exports={
 
@@ -12,10 +13,20 @@ module.exports={
             callback(data.insertedId)
         })
     },
+
     getAllProducts:() =>{                                                      // using promise
         return new Promise(async(resolve,reject)=>{
             let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
             resolve(products)                //resolve is for success 
         })                                  //when it is returned its
-    }                                      //value will be in then
+    },                                      //value will be in then
+
+    deleteProduct:(proId) =>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({_id:objectId(proId)}).then((response)=>{
+                console.log(response)
+                resolve(response)
+            })
+        })
+    }
 }
