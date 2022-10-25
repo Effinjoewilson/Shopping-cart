718821863e120e294cfa6a081aadf7b187d74efa
Effin Joe Wilson <effinjoewilson@gmail.com>
2022-10-25 14:11:25 +0530

    Cart count badge created from bootstrap designs and displayed number of items in the badge

--- routes/user.js
+++ routes/user.js
@@ -11,14 +11,19 @@ const verifyLogin=(req,res,next)=>{
 }
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',async function(req, res, next) {
 
   let user=req.session.user
   //console.log(user)
+  let cartCount=null
+  if(req.session.user){
+    cartCount=await userHelpers.getCartCount(req.session.user._id)
+  }
+  
 
   productHelpers.getAllProducts().then((products)=>{
     //console.log(products)
-    res.render('user/view-products',{admin:false,products,user}); 
+    res.render('user/view-products',{admin:false,products,user,cartCount}); 
   })
 
 });
