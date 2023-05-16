b1773b6143bba193ea923a51bebe4f80f0d6ba3f
Effinjoewilson <effinjoewilson@gmail.com>
2023-05-16 09:31:57 +0530

    Made cart.hbs responsive, moved functions to script.js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -12,3 +12,53 @@ function addToCart(proId){
       }
     })
   }
+
+
+  function changeQuantity(cartId,proId,count){
+    let quantity=parseInt(document.getElementById(proId).innerHTML)// this takes the current quantity 
+    //console.log(cartId)
+    count=parseInt(count)
+
+    $.ajax({
+        url:'/change-product-quantity',
+        data:{
+            cart:cartId,
+            product:proId,
+            count:count,
+            quantity:quantity
+        },
+        method:'post',
+        success:(response)=>{
+            if(response.removeProduct){
+                alert("Product removed from the cart")
+                //console.log("PRODUCT DELETED")
+                location.reload()
+            }else{
+                //console.log("PRPDUCT INCREMENT OR DECREMENTED")
+                document.getElementById(proId).innerHTML=quantity+count
+                location.reload()
+            }
+        }
+    })
+}
+
+function removeProduct(cartId,proId){
+
+    $.ajax({
+        url:'/remove-product',
+        data:{
+            cart:cartId,
+            product:proId
+        },
+        method:'post',
+        success:(response)=>{
+            if(response.removeProduct){
+                //console.log("PRODUCT DELETED")
+                alert("Product removed from the cart")
+                location.reload()
+            }else{
+                console.log("ERROR OCCURED")
+            }
+        }
+    })
+}
